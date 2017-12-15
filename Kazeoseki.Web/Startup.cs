b919a3bd0e5171c4c5a80e1ef512d60e1b4149f1
Nina Tran <ninatranschacc@gmail.com>
2017-12-14 16:51:13 -0800

    Added blank web project

---
+++ Kazeoseki.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kazeoseki.Web.Startup))]
+namespace Kazeoseki.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

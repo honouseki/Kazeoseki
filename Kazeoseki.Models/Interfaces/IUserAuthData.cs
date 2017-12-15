c4f50f0b96d210fbea8a2445248437d3992ad165
Nina Tran <ninatranschacc@gmail.com>
2017-12-15 02:16:09 -0800

    Skeleton projects for Data, Models, Services added

---
+++ Kazeoseki.Models/Interfaces/IUserAuthData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kazeoseki.Models
+{
+    public interface IUserAuthData
+    {
+        int UserId { get; }
+        string Username { get; }
+        string Email { get; }
+        bool Remember { get; }
+        int RoleId { get; }
+        bool Confirmed { get; }
+        bool Suspended { get; }
+    }
+}
